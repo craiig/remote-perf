2ebacef47762476e54f16fecbf2e43f48680d3cf
Craig Mustard <craiig@gmail.com>
2015-10-26 15:31:44 -0700

    first commit

---
+++ perfaws.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+"""
+Enumerate the AWS instances we want using some kind of library
+
+Three ways to use this:
+
+Setup:
+Connect to them and install perf, waiting until all machines have succeeded
+
+Measure:
+Connect to all machines and start perf
+Wait for ctrl+c from script - or until a time? or another event? maybe wait for a local process to finish!!!
+
+Collect:
+Connect to all machines and download the most recent perf log
+"""
+
+
