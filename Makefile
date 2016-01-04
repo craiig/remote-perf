3b99735a5c77618b5fe0eda9a8a2ea57668e90d2
craig <craiig@gmail.com>
2016-01-03 16:03:20 -0800

    added daemon mode, support for oracle jdk

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 # format:
 #  user@host:port
 hosts := hosts.txt
+identityfile := ../spark-tests.pem
 user := root
 
 #where the flamegraph git has been cloned
@@ -15,22 +16,41 @@ install:
 	# needed to cleanly exit perf on the remote machines
 	pip install git+https://github.com/craiig/parallel-ssh
 
+daemon-run:
+	#this command below is ridiculous but neccessary:
+	# sudo -b puts the command into the background properly BUT when pssh
+	# disconnects, HUP gets sent and sudo hasn't finished setting up yet
+	# so by adding nohup we avoid this problem!
+	./pssh.py -v -l $(user) \
+		--hosts=$(hosts) \
+		--soft_kill \
+		-i \
+		-x "-oStrictHostKeyChecking=no -i $(identityfile)" \
+		'echo started; mkdir -p perfdata; cd perfdata; pwd; nohup sudo -b perf record -F 99 -ag -o perf.data -- 2> perf_output.txt;'
+
+daemon-stop:
+	./pssh.py -v -l $(user) \
+		--hosts=$(hosts) \
+		--soft_kill \
+		-i \
+		-x "-oStrictHostKeyChecking=no -i $(identityfile)" \
+		'sudo killall perf'
+
 run:
 	./pssh.py -v -l $(user) \
 		--hosts=$(hosts) \
 		--soft_kill \
 		-i \
 		-x "-oStrictHostKeyChecking=no -i ../spark-tests.pem" \
-		'echo started; ~/jmaps; mkdir -p perfdata && cd perfdata && sudo perf record -ag -o perf.data -- 2> perf_output.txt'
-
-
+		'echo started; mkdir -p perfdata && cd perfdata && sudo perf record -F 99 -ag -o perf.data -- 2> perf_output.txt'
+		#'echo started; ~/jmaps; mkdir -p perfdata && cd perfdata && sudo perf record -F 99 -ag -o perf.data -- 2> perf_output.txt'
 get:
 	./pssh.py -v -l $(user) \
 		--hosts=$(hosts) \
 		--soft_kill \
 		-i \
 		-x "-oStrictHostKeyChecking=no -i ../spark-tests.pem" \
-		'cd perfdata && sudo chmod a+rw * && perf script | gzip > perf.out.gz'
+		'cd perfdata && sudo chmod a+rw * && perf script | gzip > perf.out.gz && tar -vzcf perf-maps.tar.gz /tmp/perf-*.map'
 
 	./pscp.py -v -l $(user) \
 		--hosts=$(hosts) \
@@ -47,7 +67,7 @@ output/perfdata/%/perf.data.folded:  output/perfdata/%/perf.out
 	$(flamegraphdir)/stackcollapse-perf.pl $< > $@
 
 output/flamegraphs/%.svg: output/perfdata/%/perf.data.folded
-	$(flamegraphdir)/flamegraph.pl $< > $@
+	$(flamegraphdir)/flamegraph.pl --color=java $< > $@
 
 output/flamegraphs/:
 	mkdir -p $@
@@ -55,3 +75,5 @@ output/flamegraphs/:
 #analyze: output/flamegraphs/%.svg
 analyze: output/flamegraphs/ $(flamegraphs)
 	
+backup-perfdata:
+	tar -vzcf perfdata-`date +%s`.tar.gz output/perfdata/
--- user_data.sh
+++ user_data.sh
@@ -1,30 +1,69 @@
 #!/usr/bin/env bash
-#we need perf
-yum install -y perf
+# run this as root!
+#
+# ***** config
+#enable the oracle jdk, this is needed to properly collect stack traces
+# if you can't install the oracle jdk, you could try recompiling openjdk
+# with -fno-omit-frame-pointer
+ORACLE=1
 
-yum -y install java-1.8.0-openjdk.x86_64
-yum -y install java-1.8.0-openjdk-devel.x86_64
+# ***** end config
+
+
+#disable ASLR so perf works?
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+
+#setup JAVA
+#remove any old java
 yum -y remove java-1.7.0-openjdk.x86_64
 yum -y remove java-1.6.0-openjdk.x86_64
-
-#some systems have hardcoded 1.7 paths
+yum -y install java-1.8.0-openjdk.x86_64
+yum -y install java-1.8.0-openjdk-devel.x86_64
+if [ $ORACLE -eq 1 ]; then
+#setup oracle java
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.rpm
+rpm -i jdk-8u66-linux-x64.rpm
+#some systems have hardcoded 1.7 paths so we fake them
+ln -sf /usr/java/latest /usr/lib/jvm/java-1.7.0
+else 
+#some systems have hardcoded 1.7 paths so we fake them
 ln -sf /usr/lib/jvm/java-1.8.0 /usr/lib/jvm/java-1.7.0
+fi
 
-#make sure frame pointers are enabled
-echo "export _JAVA_OPTIONS=\"-XX:+PreserveFramePointer\"" >> ~/.bash_profile
-echo "export JAVA_TOOL_OPTIONS=\"-XX:+PreserveFramePointer\"" >> ~/.bash_profile
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0" >> ~/.bash_profile
-echo "source ~/.bash_profile" >> ~/.bashrc
-source ~/.bash_profile
+#-----
+
+#setup perf
+yum install -y perf
 
 #setup for perf-map-agent
+yum install -y git #some machines don't come with git preloaded
+yum install -y gcc gcc-c++ #or gcc
 yum install -y cmake
 cd /usr/lib/jvm
-git clone --depth=1 https://github.com/jrudolph/perf-map-agent 
+git clone --depth=1 https://github.com/craiig/perf-map-agent 
 cd perf-map-agent
 cmake .
 make
 cd ~
+
+#make sure frame pointers are enabled and symbols are dumped for each java process
+#echo "export _JAVA_OPTIONS=\"-XX:+PreserveFramePointer\"" >> ~/.bash_profile
+echo "export JAVA_TOOL_OPTIONS=\"-XX:+PreserveFramePointer -agentpath:/usr/lib/jvm/perf-map-agent/out/libperfmap.so\"" >> ~/.bash_profile
+source ~/.bash_profile
+cat ~/.bash_profile
+
+# we replace the java at /usr/bin/java to ensure that all java invocations see
+# the environment variables
+cat > ~/java << EOF
+#!/usr/bin/env bash
+source /root/.bash_profile
+/etc/alternatives/java \$@
+EOF
+chmod +x ~/java
+#rm /usr/bin/java
+ln -sf ~/java /usr/bin/java
 
 #setup jmaps for automatically mapping running java processes stack frames
 wget https://raw.githubusercontent.com/craiig/remote-perf/master/java/jmaps
